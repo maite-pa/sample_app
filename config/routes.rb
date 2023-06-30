c6b784c071461afe1b20ffe6cac0949368f6e6c7
maite-pa <116552653+maite-pa@users.noreply.github.com>
2023-06-30 19:59:59 +0200

    --wip-- [skip ci]

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   root "static_pages#home"
-
+  get  "/help",    to: "static_pages#help"
+  get  "/about",   to: "static_pages#about"
+  get  "/contact", to: "static_pages#contact"
+  get "/users",    to: "users#new"
 end
